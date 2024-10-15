f43cdbca00c343c263c661bfc14c0337b3534509
sssashaklen <153826449+sssashaklen@users.noreply.github.com>
2024-10-15 15:02:40 +0300

    .

---
+++ lab1/GameFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace lab1
+{
+    public class GameFactory
+    {
+        public static Game CreateGame(int gameType, GameAccount? player1, GameAccount? player2)
+        {
+            switch (gameType)
+            {
+                case 1:
+                    return new BaseGame(player1, player2);
+                case 2:
+                    return new TrainingGame(player1, player2);
+                default:
+                    throw new ArgumentException("Invalid game type.");
+            }
+        }
+    }
+}
